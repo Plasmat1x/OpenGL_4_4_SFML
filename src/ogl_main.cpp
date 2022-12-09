86207f6c29526d68b7e48f3ecca44fd82b861eee
Plasmat1x <damaghack@gmail.com>
2022-12-09 17:55:51 +0700

    Triangle

--- src/ogl_main.cpp
+++ src/ogl_main.cpp
@@ -3,17 +3,18 @@
 
 #include<printf.h>
 
-int main(int argc, char **argv) 
+//int main(int argc, char **argv)
+int main()  
 {
     sf::ContextSettings settings;
     settings.depthBits = 24;
     settings.stencilBits = 8;
     settings.antialiasingLevel = 4; // Optional
-    settings.majorVersion = 4;
-    settings.minorVersion = 4;
+    settings.majorVersion = 3;
+    settings.minorVersion = 3;
     settings.attributeFlags = sf::ContextSettings::Core;
 
-    sf::Window window(sf::VideoMode(800,600), "OpenGL 4.4", sf::Style::Close, settings);
+    sf::Window window(sf::VideoMode(800,600), "OpenGL 4.4", sf::Style::Default, settings);
 
     window.setVerticalSyncEnabled(false);
 
@@ -27,6 +28,76 @@ int main(int argc, char **argv)
 
     window.setActive(true);
 
+    glViewport(0,0,800,600);
+//--------------------------------------------------------------
+
+    //https://learnopengl.com/Getting-started/Hello-Triangle
+    //init geometry
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f, 0.5f, 0.0f
+    };
+
+    //vert shd
+    const char* vertexShaderSorce = R"(#version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    })";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSorce, NULL);
+    glCompileShader(vertexShader);
+
+    //frag shd
+    const char* fragmentShaderSource = R"(#version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    })";
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //shd prog
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //VBO
+    unsigned int VBO;
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+//--------------------------------------------------------------
     while (window.isOpen())
     {
         sf::Event windowEvent;
@@ -37,19 +108,46 @@ int main(int argc, char **argv)
             case sf::Event::Closed:
                 window.close();
                 break;
-
+            
+            case sf::Event::Resized:
+                glViewport(0,0,window.getSize().x, window.getSize().y);
+                break;
             default:
                 break;
             }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+            window.close();
         }
 
         
         glClearColor(0.5,0.5,0.5,1);
         glClear(GL_COLOR_BUFFER_BIT);
+//--------------------------------------------------------------
+        //modif data in buffers
+        glBindBuffer(GL_ARRAY_BUFFER,VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        //drawing
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0,3);
 
 
 
-        //draw
+
+
+
+
+
+
+
+
+       
+//--------------------------------------------------------------
         window.display();
     }
     
